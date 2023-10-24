4c3e39f774396879352bad591472e2a97fd7b6d4
Andreas Lau Hansen <andreasLauH@outlook.com>
2023-10-24 10:52:02 +0200

    make dataset works

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,4 +1,4 @@
 import os
 import shutil
-shutil.copy('data/raw/Food Images', 'data/processed/Food Images')
+shutil.copytree('data/raw/Food Images', 'data/processed/Food Images')
 os.remove('data/processed/Food Images/pan-seared-salmon-on-baby-arugula-242445.jpg')
