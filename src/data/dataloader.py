ea63cbd451dc8f75d7c8959abcae347ad78cdc99
Andreas Lau Hansen <andreasLauH@outlook.com>
2023-11-07 11:01:57 +0100

    clean dataloader file

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -7,17 +7,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import torchvision.transforms as T
 from torch.utils.data import DataLoader, Dataset
-from tokenization import yield_tokens_title, yield_tokens_title_and_ingredients, yield_tokens_title_and_ingredients_and_instructions, get_vocab, datapipe
+from tokenization import yield_tokens_title, yield_tokens_title_and_ingredients, yield_tokens_title_and_ingredients_and_instructions, get_vocab
 from torchdata.datapipes.iter import FileOpener, IterableWrapper
 from torchtext.data.utils import get_tokenizer
 from torch.nn.utils.rnn import pad_sequence
 import ast
-
-
-NUM_CLS = 2
-VOCAB_SIZE = 50_000
-SAMPLED_RATIO = 0.2
-MAX_SEQ_LEN = 512
 
 def to_device(tensor=None):
     if tensor is None:
@@ -25,6 +19,9 @@ def to_device(tensor=None):
     return 'cuda' if tensor.is_cuda else 'cpu'
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+
 
 def set_seed(seed=1):
     random.seed(seed)
@@ -37,78 +34,16 @@ def set_seed(seed=1):
 def denormalize(tensor):
     tensor = tensor * 0.225 + 0.45
     return tensor.clamp(0, 1)
-
-class ImageDataset(Dataset):
-    def __init__(self):
-        self.path = 'data/processed/Food Images'
-        self.image_files = os.listdir(self.path)
-        self.transform = T.Compose([
-            T.Resize((224, 224)),
-            T.ToTensor(),
-            T.Normalize(mean=[0.485, 0.456, 0.406], 
-                        std=[0.229, 0.224, 0.225])
-        ])
-
-    def __len__(self):
-        return len(self.image_files)
-    
-    def __getitem__(self, idx):
-        image_path = os.path.join(self.path, self.image_files[idx])
-        image = Image.open(image_path)
-        image = self.transform(image)
-        return image
-
-class ContrastiveImageDataset(Dataset):
-    def __init__(self):
-        self.path = 'data/processed/Food Images'
-        self.image_files = os.listdir(self.path)
-        self.transform = T.Compose([
-            T.Resize((224, 224)),
-            T.ToTensor(),
-            T.Normalize(mean=[0.485, 0.456, 0.406], 
-                        std=[0.229, 0.224, 0.225])
-        ])
-    def __len__(self):
-        return len(self.image_files)
-    def random_idx(self, idx):
-        r_idx =  torch.randint(0,len(self.image_files),(1,)).item()
-        if r_idx == idx:
-            r_idx = self.random_idx(idx)
-        return r_idx
-    
-    def __getitem__(self, idx):
-        image_path = os.path.join(self.path, self.image_files[idx])
-        image = Image.open(image_path)
-        r_idx = self.random_idx(idx)
-        contrast_img = os.path.join(self.path, self.image_files[r_idx])
-        contrast_img = Image.open(contrast_img)
-        return self.transform(image), self.transform(contrast_img)
-    
-class TextDataSet(Dataset):
-    '''returns: Image_Name, title, ingredients, instructions'''
-    def __init__(self):
-        FILE_PATH = 'data/Food Ingredients and Recipe Dataset with Image Name Mapping.csv'
-        self.csv = pd.read_csv(FILE_PATH)
-
-    def __len__(self):
-        return len(self.csv)
-    
-    def tokenize(self, text):
-        return text.split()
-    
-    def __getitem__(self, idx):
-        text = self.csv.iloc[idx, :]
-        Image_Name = text.Image_Name
-        Recipe_title = text.Title
-        ingredients = text.Cleaned_Ingredients
-        instructions = text.Instructions
-        title, ingredients, instructions = self.tokenize(Recipe_title), self.tokenize(ingredients), self.tokenize(instructions)
-        return Image_Name, title, ingredients, instructions
-    
+   
 def parse_list_column(df):
     df.Cleaned_Ingredients = ast.literal_eval(df.Cleaned_Ingredients)
     df.Ingredients = ast.literal_eval(df.Ingredients)
     return df
+
+def parse_datapipe(row):
+    row[2] = ast.literal_eval(row[2])
+    row[5] = ast.literal_eval(row[5])
+    return row
 
 class CombinedDataSet(Dataset):
     '''
@@ -140,12 +75,12 @@ class CombinedDataSet(Dataset):
         FILE_PATH = 'data/Food Ingredients and Recipe Dataset with Image Name Mapping.csv'
         print('building vocab')
         self.datapipe = IterableWrapper([FILE_PATH])
-        self.datapipe = FileOpener(datapipe, mode='b')
-        self.datapipe = datapipe.parse_csv(skip_lines=1, delimiter=',', quotechar='"', quoting=1)
-        self.datapipe = datapipe.map(parse_list_column)
+        self.datapipe = FileOpener(self.datapipe, mode='b')
+        self.datapipe = self.datapipe.parse_csv(skip_lines=1, delimiter=',', quotechar='"', quoting=1)
+        self.datapipe = self.datapipe.map(parse_datapipe)
 
         self.tokenizer = get_tokenizer('basic_english')
-        self.vocab = get_vocab(datapipe, self.yield_tokens)
+        self.vocab = get_vocab(self.datapipe, self.yield_tokens)
 
         self.text_transform = lambda x: [self.vocab['']] + [self.vocab[token] for token in self.tokenizer(x)] + [self.vocab['']]
         self.csv = pd.read_csv(FILE_PATH)
@@ -156,7 +91,6 @@ class CombinedDataSet(Dataset):
     
     def __getitem__(self, idx):
         
-
         if random.random() > self.p:
             _text = self.csv.iloc[idx]
             is_pos_pair = True
@@ -184,30 +118,6 @@ def collate_batch_text(batch):
     return img, text, is_positive
 
 def main(batch_size=2):
-    # img_set = ImageDataset()
-    # img_load = DataLoader(img_set, batch_size=batch_size, shuffle=True)
-    # for img in img_load:
-    #     plt.imshow(denormalize(img[0].permute(1,2,0)))
-    #     plt.show()
-    #     break
-        
-    # contrast_data = ContrastiveImageDataset()
-    # contrast_load = DataLoader(contrast_data, batch_size=batch_size, shuffle=True)
-    # fig, ax = plt.subplots(1,2, figsize=(14, 5))
-    # for img, contrast_img in contrast_load:
-    #     ax[0].imshow(denormalize(img[0].permute(1,2,0)))
-    #     ax[1].imshow(denormalize(contrast_img[0].permute(1,2,0)))
-    #     plt.show()
-    #     break
-
-    # data_loader = TextDataSet()
-    # data_loader = DataLoader(data_loader, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
-    # for Image_Name, title, ingredients, instructions in data_loader:
-    #     print(Image_Name)
-    #     print(title)
-    #     print(ingredients)
-    #     print(instructions)
-    #     break
 
     data_set = CombinedDataSet(p=0.2, mode='train')
     data_loader = DataLoader(data_set, batch_size=batch_size, shuffle=True, collate_fn=collate_batch_text)
--- src/data/tokenization.py
+++ src/data/tokenization.py
@@ -1,27 +1,7 @@
-import random
-
-import torch
-from torch.nn.utils.rnn import pad_sequence
-from torch.utils.data import DataLoader, Sampler
-from torchdata.datapipes.iter import FileOpener, IterableWrapper
 from torchtext.data.utils import get_tokenizer
 from torchtext.vocab import build_vocab_from_iterator
-import ast
 
 # https://github.com/andrei-radulescu-banu/stat453-deep-learning-ss21/blob/main/L15/migration_tutorial.ipynb
-
-
-def parse_list_column(row):
-    row[2] = ast.literal_eval(row[2])
-    row[5] = ast.literal_eval(row[5])
-    return row
-
-FILE_PATH = 'data/Food Ingredients and Recipe Dataset with Image Name Mapping.csv'
-
-datapipe = IterableWrapper([FILE_PATH])
-datapipe = FileOpener(datapipe, mode='b')
-datapipe = datapipe.parse_csv(skip_lines=1, delimiter=',', quotechar='"', quoting=1)
-datapipe = datapipe.map(parse_list_column)
 
 tokenizer = get_tokenizer('basic_english')
 
@@ -48,72 +28,3 @@ def get_vocab(train_datapipe, yield_tokens):
     vocab.set_default_index(vocab['<EOS>'])
     return vocab
 
-vocab = get_vocab(datapipe, yield_tokens_title_and_ingredients_and_instructions)
-
-def collate_batch(batch, vocab=vocab):
-    label_list, text_list = [], []
-    text_transform = lambda x: [vocab['']] + [vocab[token] for token in tokenizer(x)] + [vocab['']]
-
-    for _text in batch:
-        # label_list.append(label_transform(_label))
-        text = _text[1] # just the title for now
-        label = int(_text[0])
-        processed_text = torch.tensor(text_transform(text))
-        text_list.append(processed_text)
-        label_list.append(label)
-
-    return torch.tensor(label_list), pad_sequence(text_list, padding_value=3.0)
-
-train_iter = datapipe
-train_dataloader = DataLoader(list(train_iter),
-                              batch_size=8,
-                              shuffle=True,
-                              collate_fn=collate_batch)
-
-
-train_iter = datapipe
-train_list = list(train_iter)
-batch_size = 8  # A batch size of 8
-
-class BatchSamplerSimilarLength(Sampler):
-    def __init__(self, dataset, batch_size, indices=None, shuffle=True):
-        self.batch_size = batch_size
-        self.shuffle = shuffle
-        # get the indices and length
-        self.indices = [(i, len(tokenizer(s[1]))) for i, s in enumerate(dataset)]
-        # if indices are passed, then use only the ones passed (for ddp)
-        if indices is not None:
-            self.indices = torch.tensor(self.indices)[indices].tolist()
-
-    def __iter__(self):
-        if self.shuffle:
-            random.shuffle(self.indices)
-
-        pooled_indices = []
-        # create pool of indices with similar lengths
-        for i in range(0, len(self.indices), self.batch_size * 100):
-            pooled_indices.extend(sorted(self.indices[i:i + self.batch_size * 100], key=lambda x: x[1]))
-        self.pooled_indices = [x[0] for x in pooled_indices]
-
-        # yield indices for current batch
-        batches = [self.pooled_indices[i:i + self.batch_size] for i in
-                range(0, len(self.pooled_indices), self.batch_size)]
-
-        if self.shuffle:
-            random.shuffle(batches)
-        for batch in batches:
-            yield batch
-
-    def __len__(self):
-        return len(self.pooled_indices) // self.batch_size
-  
-bucket_dataloader = DataLoader(train_list,
-                               batch_sampler=BatchSamplerSimilarLength(
-                                     dataset = train_list, 
-                                     batch_size=batch_size),
-                               collate_fn=collate_batch)
-
-if __name__ == '__main__':
-    print(next(iter(bucket_dataloader)))
-
-    print(next(iter(train_dataloader)))
