c0166c723a74ae39f0d964bbe29af9fb474045e7
philliphoejbjerg <philliphoejbjerg@gmail.com>
2023-11-11 12:58:00 +0100

    ready for hpc training

---
+++ job5.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#BSUB -J 5_epochs
+#BSUB -o /work3/s184984/repos/ADLCV-recipe-retrieval/sh_logs/5_epochs.out
+#BSUB -e /work3/s184984/repos/ADLCV-recipe-retrieval/sh_errors/5_epochs.err
+#BSUB -n 6
+#BSUB -q gpua100
+#BSUB -gpu 'num=1:mode=exclusive_process'
+#BSUB -W 4:00
+#BSUB -R 'rusage[mem=30GB]'
+#BSUB -R 'span[hosts=1]'
+#BSUB -B
+#BSUB -N
+
+nvidia-smi
+module load cuda/11.8
+source /work3/s184984/repos/ADLCV-recipe-retrieval/recipe/bin/activate
+
+CUDA_VISIBLE_DEVICES=0 python src/models/train_model.py \
+    --experiment_name 5_epochs \
+    --num_epochs 5 \
